76be984264b67ca5efa1b71c1d8e56b36260b6b7
Karolina Redden <dubajkaro@gmail.com>
2024-01-26 20:04:53 -0400

    allow to create a new window when there is no window active

--- main.js
+++ main.js
@@ -11,8 +11,12 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+  //mac users, alow the window to activate window there is no window 
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
-
+//for mac users, keep the app icon in the task bar when closing the window
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
