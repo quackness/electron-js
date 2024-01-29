7d1a1cd95eff87a4f3c80fb42b6cd7a051eaaf3b
Karolina Redden <dubajkaro@gmail.com>
2024-01-29 17:27:33 -0400

    add camera feed in the New video tab

--- camera.js
+++ camera.js
@@ -1,6 +1,9 @@
 /* Stream it to video element https://stackoverflow.com/questions/12024770/access-camera-from-a-browser */
 console.log(navigator.mediaDevices)
 
-const video = document.querySelector('#video');
-navigator.mediaDevices.getUserMedia({ video: true }).then(stream => console.log(stream));
+const video = document.querySelector("#camera");
+console.log(video)
+video.autoplay = true;
+
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream => video.srcObject = stream);
 
